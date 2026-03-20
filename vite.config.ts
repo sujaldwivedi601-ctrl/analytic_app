173b673a92a1d3769eae109a181c387c7dbe731a
sujaldwivedi601-ctrl <sujaldwivedi601@gmail.com>
2026-03-20 07:14:40 +0530

    Refactor vite.config.ts to include tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/analytic_app/', // This must match your GitHub repository name
-})
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/analytic_app/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
